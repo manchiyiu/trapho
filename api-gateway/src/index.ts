b22ad5e703bcfd7628a45a5a86c822573ee09f8e
ymcatar <ymcatar@gmail.com>
2017-02-21 14:25:14 +0800

    fix: sperarate build flow from start script

--- api-gateway/src/index.ts
+++ api-gateway/src/index.ts
@@ -26,16 +26,16 @@ app.get('/', async (req, res) => {
     console.log('location', result);
 
     result = await act({ role: 'timeline', cmd: 'sum', left: 1, right: 2 });
-    console.log('timeline');
+    console.log('timeline', result);
 
     result = await act({ role: 'auth', cmd: 'sum', left: 1, right: 2 });
-    console.log('auth');
+    console.log('auth', result);
 
     result = await act({ role: 'storage', cmd: 'sum', left: 1, right: 2 });
-    console.log('storage');
+    console.log('storage', result);
 
     result = await act({ role: 'activity', cmd: 'sum', left: 1, right: 2 });
-    console.log('activity');
+    console.log('activity', result);
 
     console.log('\n--------');
   } catch (e) {
--- storage-microservice/src/index.ts
+++ storage-microservice/src/index.ts
@@ -4,7 +4,7 @@ import * as seneca from 'seneca';
 const app = express();
 
 seneca()
-  .add('role:auth,cmd:sum', (msg, reply) => {
+  .add('role:storage,cmd:sum', (msg, reply) => {
     reply(null, { answer: (msg.left + msg.right) });
   })
   .listen();
