3190cb88f80df0eaf2661bb557e50b04dd0847bd
Asleep_King <King@Asleep-Kingde-MacBook-Air.local>
2017-03-14 20:39:49 +0800

    add: more field in signup page

--- frontend/src/compoments/login-page/module.js
+++ frontend/src/compoments/login-page/module.js
@@ -4,12 +4,8 @@ import Logo from './logo.vue';
 import Toolbar from './toolbar.vue';
 import LoginForm from './login-form.vue';
 import SignupForm from './signup-form.vue';
-import SignupForm2 from './signup2-form.vue';
-import wishlist from './wish-list.vue';
 
 Vue.component('login-page-logo', Logo);
 Vue.component('login-page-toolbar', Toolbar);
 Vue.component('login-page-login-form', LoginForm);
 Vue.component('login-page-signup-form', SignupForm);
-Vue.component('login-page2-signup-form', SignupForm2);
-Vue.component('wish-list', wishlist);
