03c1634280050bc0f113d0167b9b4cfdb55ffc39
ymcatar <ymcatar@gmail.com>
2017-03-31 12:04:24 +0800

    add: vr code template

--- frontend/src/compoments/photo-feed/module.js
+++ frontend/src/compoments/photo-feed/module.js
@@ -6,6 +6,7 @@ import Toolbar from './toolbar.vue';
 import TabFeed from './tabs/tab-feed.vue';
 import TabLocations from './tabs/tab-locations.vue';
 import TabUpload from './tabs/tab-upload.vue';
+import TabVR from './tabs/tab-vr.vue';
 
 /* content */
 import ContentCard from './content/content-card.vue';
@@ -16,6 +17,7 @@ Vue.component('photo-feed-toolbar', Toolbar);
 Vue.component('photo-feed-tab-feed', TabFeed);
 Vue.component('photo-feed-tab-locations', TabLocations);
 Vue.component('photo-feed-tab-upload', TabUpload);
+Vue.component('photo-feed-tab-vr', TabVR);
 
 Vue.component('photo-feed-content-card', ContentCard);
 Vue.component('photo-feed-content-card-list', ContentCardList);
