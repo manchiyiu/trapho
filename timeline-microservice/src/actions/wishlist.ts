7e056b89ee0165a9f8fe0ddf4d9d5111df2677f5
re-Kay <infinite060506@gmail.com>
2017-04-01 19:12:03 +0800

    Fix: like retrieve bug

--- timeline-microservice/src/actions/wishlist.ts
+++ timeline-microservice/src/actions/wishlist.ts
@@ -1,16 +1,19 @@
 import Like from '../model';
 import { act, isValidUser, isValidPhoto } from '../utils';
+import likeRetrieve from './like-retrieve';
 
 export default async (msg, reply) => {
   const { userId } = msg;
   try{
-    let { likes } = await act({role: 'timeline', cmd: 'likeRetrieve', userId});
+    await isValidUser(userId);
+    let likes = await Like.retrieveByUserId(userId);
     likes = await likes.map(async item => {
       const { id , userId, photoId } = item;
       let { locationId } =  await act({ role: 'photo', cmd: 'photoRetrieve', photoId });
       let wish = { 'likeId':id, photoId, locationId };
       return wish;
     });
+    console.log(likes);
     reply(null, { 'wishlist': likes } );
   } catch(err){
     reply(err, null);
