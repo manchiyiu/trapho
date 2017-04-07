a2c630ac403e79d308c2ce067a8ea7401caaa763
warren10hk <warren10hk@yahoo.com.hk>
2017-04-08 01:35:08 +0800

    Add: CSS alignment and padding

--- frontend/src/compoments/edit-profile/view.vue
+++ frontend/src/compoments/edit-profile/view.vue
@@ -1,45 +1,47 @@
 <name>edit-view</name>
 
 <template>
-  <div class="main">
-    <md-card class="card">
-      <md-card-content>
-        <form novalidate @submit.stop.prevent="submit">
-
-         
-          <md-input-container class="input-container">
-            <md-icon>vpn_key</md-icon>
-            <label>Original Password</label>
-            <md-input required v-model="password" type="password"></md-input>
-          </md-input-container>
+  <div style="padding: 16px">
+    <div class="main">
+      <md-card class="card">
+        <md-card-content>
+          <form novalidate @submit.stop.prevent="submit">
 
           
-          <md-input-container class="input-container">
-            <md-icon>vpn_key</md-icon>
-            <label>New Password</label>
-            <md-input v-model="newpassword" type="password"></md-input>
-          </md-input-container>
+            <md-input-container class="input-container">
+              <md-icon>vpn_key</md-icon>
+              <label>Original Password</label>
+              <md-input required v-model="password" type="password"></md-input>
+            </md-input-container>
 
-          <md-input-container class="input-container">
-            <md-icon>vpn_key</md-icon>
-            <label>Re-enter New Password</label>
-            <md-input v-model="newpassword2" type="password"></md-input>
-          </md-input-container>
+            
+            <md-input-container class="input-container">
+              <md-icon>vpn_key</md-icon>
+              <label>New Password</label>
+              <md-input v-model="newpassword" type="password"></md-input>
+            </md-input-container>
 
-          <md-input-container class="input-container">
-            <md-icon>email</md-icon>
-            <label>Email</label>
-            <md-input v-model="email"></md-input>
-          </md-input-container>
+            <md-input-container class="input-container">
+              <md-icon>vpn_key</md-icon>
+              <label>Re-enter New Password</label>
+              <md-input v-model="newpassword2" type="password"></md-input>
+            </md-input-container>
 
-          <md-button class="md-primary md-raised" :disabled="!isFilled" @click.native="submit">Submit</md-button>
-          
-        </form>
-      </md-card-content>
-    </md-card>
-    <md-snackbar md-position="bottom center" ref="snackbar" :md-duration="4000">
-      <span>{{ errorMessage }}</span>
-    </md-snackbar>
+            <md-input-container class="input-container">
+              <md-icon>email</md-icon>
+              <label>Email</label>
+              <md-input v-model="email"></md-input>
+            </md-input-container>
+
+            <md-button class="md-primary md-raised" :disabled="!isFilled" @click.native="submit">Submit</md-button>
+            
+          </form>
+        </md-card-content>
+      </md-card>
+      <md-snackbar md-position="bottom center" ref="snackbar" :md-duration="4000">
+        <span>{{ errorMessage }}</span>
+      </md-snackbar>
+    </div>
   </div>
 </template>
 
