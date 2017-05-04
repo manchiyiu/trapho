94dece82824798002844104e998785c1dd69c961
ymcatar <ymcatar@gmail.com>
2017-05-05 00:09:18 +0800

    fix: removed dependency

--- frontend/src/compoments/photo-feed/module.js
+++ frontend/src/compoments/photo-feed/module.js
@@ -4,12 +4,10 @@ import Toolbar from './toolbar.vue';
 
 /* tabs */
 import TabFeed from './tabs/tab-feed.vue';
-import TabLocations from './tabs/tab-locations.vue';
 import TabUpload from './tabs/tab-upload.vue';
 
 // register all the components within this module
 Vue.component('photo-feed-toolbar', Toolbar);
 
 Vue.component('photo-feed-tab-feed', TabFeed);
-Vue.component('photo-feed-tab-locations', TabLocations);
 Vue.component('photo-feed-tab-upload', TabUpload);
